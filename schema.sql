2cdbfb4ea60f8589a579fcb8cd37b2eedc90e9f0
Manish Boddu <2320030082@klh.edu.in>
2026-07-31 19:10:11 +0530

    Initialize Student Management System database schema

---
+++ schema.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE StudentManagementSystem;
+
+USE StudentManagementSystem;
+
+CREATE TABLE Students (...);
+
+CREATE TABLE Courses (...);
+
+CREATE TABLE Faculty (...);
+
+CREATE TABLE Enrollments (...);
